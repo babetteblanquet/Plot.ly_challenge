18171c352f724c5f41156e506fe33c7f61b6ebe2
babetteblanquet <65524644+babetteblanquet@users.noreply.github.com>
2020-08-31 16:00:16 +1000

    H bar finalised

--- static/js/app.js
+++ static/js/app.js
@@ -1,62 +1,68 @@
-// function unpack(rows, index) {
-//     return rows.map(function(row) {
-//       return row[index];
-//     });
-//   }
-
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("samples.json").then(function(data) {
-    console.log(typeof(data));;
+    // console.log(typeof(data));;
     // Grab sample_values, otu_ids and otu_labels from the response json object to build the plots
+    var sample_data = data.samples[0]
     var sample_values = data.samples[0].sample_values;
     var otu_ids = data.samples[0].otu_ids;
     var otu_labels = data.samples[0].otu_labels;
 
-    console.log(sample_values)
-    console.log(otu_ids)
-    console.log(otu_labels)
+    // console.log(sample_data)
+    // console.log(sample_values)
+    // console.log(otu_ids)
+    // console.log(otu_labels)
       
-// // Use D3 fetch to read the JSON file
-// // The data from the JSON file is arbitrarily named importedData as the argument
-// d3.json("samples.json").then((importedData) => {
-//     console.log(importedData);
-//     var data = importedData;
+    //Sort the sample_values array
+    var sortedData = sample_values.sort((a, b) => b - a);
+    console.log(sortedData)
+    // Slice the first 10 objects for plotting
+    var slicedData = sortedData.slice(0, 10);
+    var slicedIDs = otu_ids.slice(0,10);
+    var slicedLabels = otu_labels.slice(0,10);
+    //console.log(slicedData)
 
-//     // Sort the data array using the sample values
-//     var sortedData = data.sort((a, b) => b.sample_values - a.sample_values);
+    // Reverse the array due to Plotly's defaults
+    var reverseData = slicedData.reverse();
+    var reverseIDs = slicedIDs.reverse();
+    var reverseLabels = slicedLabels.reverse();
+    // console.log(reverseData)
+    // console.log(reverseIDs)
+    // console.log(reverseLabels)
+
+    //Concatenate "OTU" for each otu_ids number to create an ID array with strings such as OTU_1977
+    var IDs = [];
+    // Iterate through each ID object
+        Object.values(reverseIDs).forEach(value => {
+            // Concatenate "OTU" with each ID number
+            IDs.push(('OTU_').concat(value))
+        });
     
-//     // Slice the first 10 objects for plotting
-//     var slicedData = data.slice(0, 10);
+    console.log(IDs);
+  
+    // Trace1 for the sameple values Data
+    var trace1 = {
+        x: reverseData,
+        y: IDs,
+        text: reverseLabels,
+        name: "OTUs",
+        type: "bar",
+        orientation: "h"
+    };
 
-//     // Reverse the array due to Plotly's defaults
-//     var reverseData = data.reverse();
-    
-//     // Trace1 for the Greek Data
-//     var trace1 = {
-//         x: reversedData.map(object => object.samples_values),
-//         y: reversedData.map(object => object.otu_ids),
-//         text: reversedData.map(object => object.otu_labels),
-//         name: "OTUs",
-//         type: "bar",
-//         orientation: "h"
-//     };
+    // data
+    var data = [trace1];
 
-//     // data
-//     var data = [trace1];
-
-//     // Apply the group bar mode to the layout
-//     var layout = {
-//         title: "Top 10 OTUs (operational taxonomic units) in belly button",
-//        margin: {
-//             l: 100,
-//             r: 100,
-//             t: 100,
-//             b: 100
-//         }
-//     };
-
-//     // Render the plot to the div tag with id "plot"
-//     Plotly.newPlot("bar", data, layout);
-
+    // Apply the group bar mode to the layout
+    var layout = {
+        title: "Top 10 OTUs (operational taxonomic units) in belly button",
+       margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+        }
+    };
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bar", data, layout);
 });
